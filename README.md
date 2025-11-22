c318623fb7505454721cebdf8082f414a73be561
Ian Li <ianayl.work@gmail.com>
2025-11-21 22:11:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simon says
+
+Simon says go away right now I am not done
---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> {
+    overlays = [
+      (self: super: {
+        opencv4 = super.opencv4.override {
+          enableGtk2 = true;
+          enableGtk3 = true;
+          enablePython = true;
+        };
+      })
+    ];
+  }
+}:
+
+let
+  pythonWithPackages = pkgs.python3.withPackages (python: [
+    python.opencv4 # We've overlayed opencv4
+    python.numpy
+    python.pandas
+    python.requests
+  ]);
+  buildInputAlias = [
+    pythonWithPackages
+    pkgs.python3Packages.pip
+  ];
+in
+pkgs.stdenv.mkDerivation {
+  name = "bullshit";
+  buildInputs = buildInputAlias;
+
+  shellHook = ''
+    # source venv/bin/activate
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputAlias}:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH"
+    export PATH="${pythonWithPackages}/bin:$PATH"
+  '';
+}
